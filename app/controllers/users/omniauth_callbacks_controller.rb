58e94fcb6ff805720e9a6eff35f6705ea04895d2
Arjav Jain <arjavjain28041998@gmail.com>
2021-05-26 13:09:45 +0530

    v

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,7 +11,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     else
       user = User.find_by(email: @user.email)
       if user.present?
-        sign_in_and_redirect user , event: :authentication
+        sign_in_and_redirect @user , event: :authentication
       else
         session["devise.google_data"] = request.env["omniauth.auth"].except(:extra) 
         redirect_to new_user_registration_url
@@ -23,12 +23,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+      sign_in_and_redirect @user, event: :authentication 
       set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
     else
       user = User.find_by(email: @user.email)
       if user.present?
-        sign_in_and_redirect user , event: :authentication
+        sign_in_and_redirect @user , event: :authentication
       else
         session["devise.google_data"] = request.env["omniauth.auth"].except(:extra) 
         redirect_to new_user_registration_url
@@ -44,7 +44,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     else
      user = User.find_by(email: @user.email)
       if user.present?
-        sign_in_and_redirect user , event: :authentication
+        sign_in_and_redirect @user , event: :authentication
       else
         session["devise.google_data"] = request.env["omniauth.auth"].except(:extra) 
         redirect_to new_user_registration_url
@@ -60,7 +60,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     else
       user = User.find_by(email: @user.email)
       if user.present?
-        sign_in_and_redirect user , event: :authentication
+        sign_in_and_redirect @user , event: :authentication
       else
         session["devise.google_data"] = request.env["omniauth.auth"].except(:extra) 
         redirect_to new_user_registration_url
